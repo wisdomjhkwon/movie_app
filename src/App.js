32f125f2c6f23a6dae41dc0a8fcb553bf6044947
wisdomjhkwon <wisdomjhkwon@gmail.com>
2019-10-22 21:52:59 +0900

    #2.4 Protection with Proptypes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Food({name, pic}) {
+function Food({name, pic, rating}) {
   return <div>
      <h2>I like {name}</h2>
-     <img src={pic} alt={name}/>
+     <h4> {rating} / 5.0 </h4>
+     <img src={pic} alt={name} />
      </div>;
 }
 
+// MUST BE "propTypes"
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  pic: PropTypes.string.isRequired,
+  rating: PropTypes.number // can be "undefined"
+};
+
 const foodILike = [
-  { name: "Pasta", image: "http://www.venicecafechicago.com/wp-content/uploads/2014/07/venicea01.jpg"},
-  { name: "cake", image: "https://fthmb.tqn.com/upIR2sGv4idGaLiTQHGc6gXZy78=/1280x853/filters:fill(auto,1)/82662502-56a2145f5f9b58b7d0c661ab.jpg"},
-  { name: "Kimbap", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Vegetable_gimbap.jpg/1200px-Vegetable_gimbap.jpg"}
-];
+  { id:1, name: "Pasta", rating: 5, image: "http://www.venicecafechicago.com/wp-content/uploads/2014/07/venicea01.jpg"},
+  { id:2, name: "cake", rating: 4.9, image: "https://fthmb.tqn.com/upIR2sGv4idGaLiTQHGc6gXZy78=/1280x853/filters:fill(auto,1)/82662502-56a2145f5f9b58b7d0c661ab.jpg"},
+  { id:3, name: "Kimbap", rating: 3.5, image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Vegetable_gimbap.jpg/1200px-Vegetable_gimbap.jpg"}
+]
 
-function renderFood(dish) {
-  return <Food name={dish.name} pic={dish.image} />
-}
-
+// id: react internal use
 function App() {
   return <div> 
-    {foodILike.map(renderFood)}
+    {foodILike.map(dish => <Food key={dish.id} name={dish.name} pic={dish.image} rating={dish.rating}/>)} 
     </div> ;
 }
 
